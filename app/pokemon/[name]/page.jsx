f5a38c2ff01f46f84cd0e78b6f3969fadd906d3e
Muhammad Faisal <muh.faisal572@gmail.com>
2025-11-15 14:14:55 +0700

    init : initial project

---
+++ app/pokemon/[name]/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+
+const PokemonSearch = ({ params }) => {
+  console.log("parms", params);
+  return <div>PokemonSearch</div>;
+};
+
+export default PokemonSearch;
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
+      ),
+    ],
+  },
 };
 
 export default nextConfig;
